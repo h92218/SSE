a290ee77a6e87f935d7251cf13b6ec54e7940f00
현선 박 <sh717510@gmail.com>
2024-05-24 15:11:42 +0900

    send test complete

--- src/main/java/kr/co/dominos/demo/controller/SseController.java
+++ src/main/java/kr/co/dominos/demo/controller/SseController.java
@@ -1,4 +1,37 @@
 package kr.co.dominos.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.dominos.demo.service.SseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/SSE")
 public class SseController {
+
+    private final SseService sseService;
+
+    @GetMapping(value = "/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(@PathVariable String id) {
+        //생성자를 통해 만료시간 설정가능. 스프링 부트 내장톰캣 30초
+        //만료시간이 되면 브라우저에서 자동으로 서버에 재연결 요청
+        SseEmitter emitter = sseService.connect(id);
+
+        //클라이언트와의 SSE연결을 설정하고 데이터를 전송하기 위한 스트림 생성
+        return ResponseEntity.ok(emitter);
+    }
+
+    //메세지 보내기 테스트용
+    @PostMapping(value = "/sendTest/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public void sendTest(@PathVariable String id, HttpServletRequest request) {
+        sseService.sendToClient(id,request.getParameter("orderId"),request.getParameter("status"));
+    }
+
+
 }
--- src/main/java/kr/co/dominos/demo/service/SseService.java
+++ src/main/java/kr/co/dominos/demo/service/SseService.java
@@ -1,4 +1,56 @@
 package kr.co.dominos.demo.service;
 
+import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Log4j2
+@Service
 public class SseService {
+    //동시성이 요구되는 환경에서는 ConcurrentHashMap을 사용하여 스레드 안전성을 보장하는 것이 좋다고 함
+    private final Map<String,SseEmitter> emitters = new ConcurrentHashMap<>();
+
+
+    //연결시도 수신 및 메세지 전송
+    public SseEmitter connect(String id){
+        SseEmitter emitter = new SseEmitter();
+
+        //사용자 연결 보관
+        emitters.put(id,emitter);
+
+        try{
+            emitter.send(SseEmitter.event().id(id).data("[" + id + "] 님 환영합니다."));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return emitter;
+    }
+
+    //메세지 전송
+    public void sendToClient(String id,String orderId, String status){
+        SseEmitter emitter = emitters.getOrDefault(id,null);
+
+        JSONObject jb = new JSONObject();
+        jb.put("orderId",orderId);
+        jb.put("status",status);
+
+        if(emitter != null){
+            try{
+                emitter.send(SseEmitter.event().id(id).name("message").data(jb));
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            log.info("[" + id + "] 는 연결 목록에 없는 사용자");
+
+        }
+
+    }
+
 }
